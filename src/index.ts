cea6fb790a802a79b472d02b4eb35ac3b69f4da9
Roberto Nazareth <nazarethguedesroberto@gmail.com>
2022-04-19 08:32:18 -0300

    Fazer a página inicial

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,11 @@ const app = express();
 const port = 3000;
 
 app.set('view engine', 'ejs');
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
     res.render('index', {
-        titulo: 'Encurtador de URL',
+        titulo: '',
         nome: 'mundo'
     });
 });
